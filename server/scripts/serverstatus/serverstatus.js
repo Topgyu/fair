d6ca9803a3aa0d9c2477f06f8fc238624bd02366
Topgyu <teakgyu90@naver.com>
2015-09-19 02:06:27 +0900

    bug fix

--- server/scripts/serverstatus/serverstatus.js
+++ server/scripts/serverstatus/serverstatus.js
@@ -10,10 +10,13 @@ function get_failed_server(callback){
 				}
 			}
 			
-			$('name').removeClass('red');
+			$('.name').removeClass('red');
 			for (var i = 0 ; i < server_list.length ; i++) {
 				$('.' + server_list[i]).addClass('red');
 				$('title')[0].text = '* Server Status';
+			}
+			if (server_list.length == 0){
+				$('title')[0].text = 'Server Status';
 			}
 		}
 	});
@@ -27,10 +30,10 @@ function t(x){
 $(function(){
 	// initialize
 	for (var i = 1; i <= 18 ; i++){
-		$('<li></li>').addClass('name').addClass('c' + t(i)).text('c' + t(i)).appendTo('#status').addClass('green');
+		$('<li></li>').addClass('name').addClass('c' + t(i)).text('c' + t(i)).appendTo('#status');
 	}
 	for (var i = 1; i <= 4 ; i++){
-		$('<li></li>').addClass('name').addClass('x' + t(i)).text('x' + t(i)).appendTo('#status').addClass('green');
+		$('<li></li>').addClass('name').addClass('x' + t(i)).text('x' + t(i)).appendTo('#status');
 	}
 	get_failed_server();
 
